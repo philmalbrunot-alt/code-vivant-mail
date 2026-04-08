7979e5894f282a52602c31ab1b3c86c457b4e237
philmalbrunot-alt <phil.malbrunot@gmail.com>
2026-04-08 10:58:30 +0200

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,8 +5,9 @@ let stripeSingleton: Stripe | null = null;
 export function getStripe() {
   if (!stripeSingleton) {
     stripeSingleton = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-      apiVersion: '2025-02-24.acacia',
+      apiVersion: '2025-07-30.basil',
     });
   }
+
   return stripeSingleton;
 }
