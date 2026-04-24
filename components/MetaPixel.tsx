5b3c213130db61491cf5712817200abec5d3d03a
philmalbrunot-alt <phil.malbrunot@gmail.com>
2026-04-24 15:19:05 +0200

    Create MetaPixel.tsx

---
+++ components/MetaPixel.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import Script from 'next/script'
+import { usePathname } from 'next/navigation'
+import { useEffect } from 'react'
+
+declare global {
+  interface Window {
+    fbq?: (...args: any[]) => void
+  }
+}
+
+const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID
+
+export default function MetaPixel() {
+  const pathname = usePathname()
+
+  useEffect(() => {
+    if (!META_PIXEL_ID) return
+    if (typeof window === 'undefined') return
+    if (!window.fbq) return
+
+    window.fbq('track', 'PageView')
+  }, [pathname])
+
+  if (!META_PIXEL_ID) return null
+
+  return (
+    <>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${META_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
+  )
+}
